0476df8d54aa6b4b8ef608514a0c791937d1431b
Wei1013 <71314057+Wei1013@users.noreply.github.com>
2021-05-21 14:16:35 +0800

    2021上機考v3_(于大惟)

--- app/src/main/java/tw/edu/pu/s1071479/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1071479/crazyshape/MainActivity.kt
@@ -6,17 +6,34 @@ import android.os.Bundle
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener, View.OnTouchListener {
     lateinit var gDetector: GestureDetector
+    var PictureNo:Int = 0  //目前顯示第幾張圖
+    var TotalPictures:Int = 4 //總共幾張圖片(假設僅顯示pu0-pu3)
+
+
+    fun ShowPicture(){
+
+        when (PictureNo){
+            0 -> imgNext.setImageResource(R.drawable.circle)
+            1 -> imgNext.setImageResource(R.drawable.square)
+            2 -> imgNext.setImageResource(R.drawable.star)
+            3 -> imgNext.setImageResource(R.drawable.triangle)
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         gDetector = GestureDetector(this, this)
         imgNext.setOnTouchListener(this)
         Toast.makeText(this, "作者:于大惟", Toast.LENGTH_SHORT).show();
+
+
+
     }
 
     override fun onDown(p0: MotionEvent?): Boolean {
@@ -28,7 +45,11 @@ class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener, Vie
     }
 
     override fun onSingleTapUp(p0: MotionEvent?): Boolean {
+        var X: Int = (0..3).random()
+        PictureNo = X
+        ShowPicture()
         return true
+
     }
 
     override fun onScroll(
